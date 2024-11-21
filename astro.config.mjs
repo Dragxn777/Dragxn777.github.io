7a542ea848079c6a3dde712eae745bf8488bc1c7
Dragxn777 <draganturk24@gmail.com>
2024-11-21 08:52:10 +0100

    maj2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dragxn777.github.io',
+    site: 'https://Dragxn777.github.io',
     integrations: [mdx(), sitemap(), tailwind()],
 
     devToolbar: {
