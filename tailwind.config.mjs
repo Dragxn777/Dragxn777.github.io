7dcf6865ff85d75e6e8f4c8fb15e369119ccae16
Dragxn777 <draganturk24@gmail.com>
2024-11-21 08:35:20 +0100

    maj

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			"main": "#2d2b28",
+		},
+	},
+	plugins: [],
+}
