b91fcb9f4f91acd14713664367f25924d125930f
Dragxn777 <draganturk24@gmail.com>
2024-11-21 08:45:27 +0100

    seturl

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://dragxn777.github.io',
     integrations: [mdx(), sitemap(), tailwind()],
 
     devToolbar: {
