06450d2797958d75ec00090aafbd734101201134
Dragxn777 <draganturk24@gmail.com>
2024-12-02 08:44:30 +0100

    V4

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,12 +3,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			"main": "#2d2b28",
+			
 		},
 
 		backgroundImage: {
 			"realmd": "url('/bg-real-md.png')",
+			"linear-pink" : "linear-gradient(180deg, rgba(84,3,18,1) 0%, rgba(45,43,40,1) 50%);",
 		},
+
+	
 	},
 	plugins: [],
 }
